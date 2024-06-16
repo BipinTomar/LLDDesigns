06fff906d4635c992350b2e53fac05dd777e1898
Bipin <bkumar_be16@thapar.edu>
2024-06-16 18:10:43 +0530

    second commit

--- OOPS/src/main/java/org/example/basicstrong/oop/LLD/SplitwiseDesign/Main.java
+++ OOPS/src/main/java/org/example/basicstrong/oop/LLD/SplitwiseDesign/Main.java
@@ -3,7 +3,9 @@ package org.example.basicstrong.oop.LLD.SplitwiseDesign;
 public class Main {
     public static void main (String[] args)
     {
-        System.out.println("Hello");
+        Splitwise splitwise = new Splitwise();
+        splitwise.demo();
+
 
     }
 }
